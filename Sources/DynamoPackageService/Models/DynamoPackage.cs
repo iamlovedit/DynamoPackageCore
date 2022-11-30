2af45d99b0ccc8c0693ead2090728e43cc506110
Young <iamlovedit@163.com>
2022-11-30 14:35:57 +0800

    init projects

---
+++ Sources/DynamoPackageService/Models/DynamoPackage.cs
@@ -0,0 +1,17 @@
+﻿using SqlSugar;
+
+namespace DynamoPackageService.Models
+{
+    public class DynamoPackage
+    {
+        [SugarColumn(IsIdentity = true, IsPrimaryKey = true)]
+        public int Id { get; set; }
+
+        [SugarColumn(IsNullable = false)]
+        public string Name { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+    }
+}
