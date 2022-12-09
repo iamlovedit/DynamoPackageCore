56aa757e98f3252c4261be6067193bf43f93285a
Young <iamlovedit@163.com>
2022-12-09 16:04:11 +0800

    update codes

--- Sources/DynamoPackageService/Jobs/QueryPackagesJob.cs
+++ Sources/DynamoPackageService/Jobs/QueryPackagesJob.cs
@@ -1,5 +1,10 @@
 ﻿using DynamoPackageService.Infrastructure;
+using DynamoPackageService.Models;
+using DynamoPackageService.UnitOfWork;
+using Newtonsoft.Json.Linq;
 using Quartz;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
 
 namespace DynamoPackageService.Jobs;
 
@@ -7,27 +12,55 @@ namespace DynamoPackageService.Jobs;
 public class QueryPackagesJob : IJob
 {
     private readonly AppDbContext _appDbContext;
+    private readonly IUnitOfWork _unitOfWork;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<QueryPackagesJob> _logger;
 
-    public QueryPackagesJob(AppDbContext appDbContext, IHttpClientFactory httpClientFactory, ILogger<QueryPackagesJob> logger)
+    public QueryPackagesJob(AppDbContext appDbContext, IUnitOfWork unitOfWork, IHttpClientFactory httpClientFactory, ILogger<QueryPackagesJob> logger)
     {
         _appDbContext = appDbContext;
+        _unitOfWork = unitOfWork;
         _httpClientFactory = httpClientFactory;
         _logger = logger;
     }
 
     public async Task Execute(IJobExecutionContext context)
     {
-        await Task.Run(() =>
+        var httpClient = _httpClientFactory.CreateClient();
+        var responseMessage = await httpClient.GetAsync("https://dynamopackages.com/");
+
+        if (responseMessage.IsSuccessStatusCode)
         {
-            
-        });
-        //return Task.Run(() =>
-        //{
-        //    //var httpClient = _httpClientFactory.CreateClient();
-        //    //var responseMessage = await httpClient.GetAsync("https://dynamopackages.com/");
-        //    _logger.LogInformation("HelloWorld");
-        //});
+            var json = await responseMessage.Content.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(json))
+            {
+                var jObject = JObject.Parse(json);
+                var content = jObject["content"];
+                if (!string.IsNullOrEmpty(content?.ToString()))
+                {
+                    var newPacakges = content.ToObject<List<DynamoPackage>>();
+                    var client = _appDbContext.GetEntityDB<DynamoPackage>();
+                    var oldPackages = await client.GetListAsync();
+                    try
+                    {
+                        _unitOfWork.BeginTransaction();
+                        foreach (var package in newPacakges)
+                        {
+                            if (!oldPackages.Any(p => p.Id == package.Id))
+                            {
+                                await client.InsertAsync(package);
+                            }
+                        }
+                        _unitOfWork.CommitTransaction();
+                    }
+                    catch (Exception e)
+                    {
+                        _unitOfWork.RollbackTransaction();
+                        _logger.LogError(e, e.Message);
+                    }
+                }
+            }
+        }
     }
+
 }
